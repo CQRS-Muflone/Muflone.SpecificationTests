1cad7f6a7ee57e2098d3ed3673e48501a1379672
Alessandro Colla <eglathon@gmail.com>
2024-05-15 17:43:22 +0200

    Implements new IRepository interface from Muflone 8.0

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # Muflone.SpecificationTests
+
 Helper for creating specification tests with Muflone
- 
-### Install ###
+
+## Install
+
 `Install-Package Muflone.SpecificationTests`
 
-### Sample usage ###
+## Sample usage
+
 Look at [this repo](https://github.com/CQRS-Muflone/CQRS-ES_testing_workshop)
--- src/Muflone.SpecificationTests/InMemoryEventRepository.cs
+++ src/Muflone.SpecificationTests/InMemoryEventRepository.cs
@@ -2,62 +2,60 @@
 using Muflone.Persistence;
 
 namespace Muflone.SpecificationTests
-{ /// <summary>
-  /// https://github.com/luizdamim/NEventStoreExample/tree/master/NEventStoreExample.Test
-  /// </summary>
-    public class InMemoryEventRepository : IRepository
-    {
-        private IEnumerable<DomainEvent> givenEvents = Enumerable.Empty<DomainEvent>();
-        public IEnumerable<DomainEvent> Events { get; private set; } = Enumerable.Empty<DomainEvent>();
+{
+	public class InMemoryEventRepository : IRepository
+	{
+		private IEnumerable<DomainEvent> givenEvents = Enumerable.Empty<DomainEvent>();
+		public IEnumerable<DomainEvent> Events { get; private set; } = Enumerable.Empty<DomainEvent>();
 
-        private static TAggregate ConstructAggregate<TAggregate>()
-        {
-            return (TAggregate)Activator.CreateInstance(typeof(TAggregate), true);
-        }
+		private static TAggregate ConstructAggregate<TAggregate>()
+		{
+			return (TAggregate)Activator.CreateInstance(typeof(TAggregate), true);
+		}
 
-        public void Dispose()
-        {
-            // no op
-        }
+		public void Dispose()
+		{
+			// no op
+		}
 
-        public virtual void ApplyGivenEvents(IList<DomainEvent> events)
-        {
-            givenEvents = events;
-        }
+		public virtual void ApplyGivenEvents(IList<DomainEvent> events)
+		{
+			givenEvents = events;
+		}
 
-        public async Task<TAggregate> GetByIdAsync<TAggregate>(Guid id) where TAggregate : class, IAggregate
-        {
-            return await GetByIdAsync<TAggregate>(id, 0);
-        }
+		public async Task<TAggregate> GetByIdAsync<TAggregate>(Guid id, CancellationToken cancellationToken) where TAggregate : class, IAggregate
+		{
+			return await GetByIdAsync<TAggregate>(id, 0, cancellationToken);
+		}
 
-        public Task<TAggregate> GetByIdAsync<TAggregate>(Guid id, int version) where TAggregate : class, IAggregate
-        {
-            var aggregate = ConstructAggregate<TAggregate>();
-            givenEvents.ForEach(aggregate.ApplyEvent);
-            return Task.FromResult(aggregate);
-        }
+		public Task<TAggregate> GetByIdAsync<TAggregate>(Guid id, long version, CancellationToken cancellationToken) where TAggregate : class, IAggregate
+		{
+			var aggregate = ConstructAggregate<TAggregate>();
+			givenEvents.ForEach(aggregate.ApplyEvent);
+			return Task.FromResult(aggregate);
+		}
 
-        public async Task SaveAsync(IAggregate aggregate, Guid commitId)
-        {
-            await SaveAsync(aggregate, commitId, null);
-        }
+		public async Task SaveAsync(IAggregate aggregate, Guid commitId, CancellationToken cancellationToken = default)
+		{
+			await SaveAsync(aggregate, commitId, null);
+		}
 
-        public Task SaveAsync(IAggregate aggregate, Guid commitId, Action<IDictionary<string, object>> updateHeaders)
-        {
-            Events = aggregate.GetUncommittedEvents().Cast<DomainEvent>();
-            return Task.CompletedTask;
-        }
-    }
+		public Task SaveAsync(IAggregate aggregate, Guid commitId, Action<IDictionary<string, object>> updateHeaders, CancellationToken cancellationToken = default)
+		{
+			Events = aggregate.GetUncommittedEvents().Cast<DomainEvent>();
+			return Task.CompletedTask;
+		}
+	}
 
-    public static class Helpers
-    {
-        //Stolen from Castle.Core.Internal
-        public static void ForEach<T>(this IEnumerable<T> items, Action<T> action)
-        {
-            if (items == null)
-                return;
-            foreach (T obj in items)
-                action(obj);
-        }
-    }
+	public static class Helpers
+	{
+		//Stolen from Castle.Core.Internal
+		public static void ForEach<T>(this IEnumerable<T> items, Action<T> action)
+		{
+			if (items == null)
+				return;
+			foreach (T obj in items)
+				action(obj);
+		}
+	}
 }
